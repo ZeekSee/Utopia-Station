a5b12369498950a3a43fc6482f26d2fb789aa2e6
Biegemot <66844527+Biegemot@users.noreply.github.com>
2023-06-24 19:00:29 +0700

    Update nanomap-renderer-invoker.sh

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-chmod +x ./tools/github-actions/nanomap-renderer-invoker.sh
-chmod +rw ./data/nanomaps
-chmod +rw .icons/_nanomaps
 # Generate maps
 tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files/cyberiad/cyberiad.dmm"
 tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files/Delta/delta.dmm"
