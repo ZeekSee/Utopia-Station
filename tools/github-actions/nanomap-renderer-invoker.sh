964e6ef0887aba85cdcacd234c24da49d6ed3937
Biegemot <66844527+Biegemot@users.noreply.github.com>
2023-06-25 15:46:30 +0700

    Revert "Update nanomap-renderer-invoker.sh"
    This reverts commit 3976d0a9b521807f83630bde91aa38bfdd05c666.

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -4,8 +4,8 @@ tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files
 tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files/Delta/delta.dmm"
 tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files/cerestation/cerestation.dmm"
 # Move and rename files so the game understands them
-cd "icons/_nanomaps"
-chmod +x icons/_nanomaps
+cd "data/nanomaps"
+chmod +x data/nanomaps
 mv "utopia_nanomap_z1.png" "Utopia_nanomap_z1.png"
 mv "delta_nanomap_z1.png" "Delta_nanomap_z1.png"
 mv "cerestation_nanomap_z1.png" "Cerestation_nanomap_z1.png"
